d107acdfc64a05f53a6f1d29a9a46963eeeab286
LordPoldi <77438745+LordPoldi@users.noreply.github.com>
2021-01-21 08:37:01 +0100

    Update index.js

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -1,2 +1,6 @@
-if (process.env.MYSQL_HOST) module.exports = require('./mysql');
-else module.exports = require('./sqlite');
+  
+if (process.env.MYSQL_HOST) {
+    module.exports = require('./mysql');
+} else if(process.env.PG_HOST) {
+    module.exports = require('./pg');
+} else module.exports = require('./sqlite');
